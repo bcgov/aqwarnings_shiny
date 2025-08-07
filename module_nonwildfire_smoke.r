c716c0c7296831e7a227c484efd6e11f08d7388a
saksjain <sakshi.jain@gov.bc.ca>
2025-08-07 14:21:35 -0700

    added copyright info

--- module_nonwildfire_smoke.r
+++ module_nonwildfire_smoke.r
@@ -10,7 +10,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
-## Module: issue Air Quality Warning - Non Wildfire Smoke
+## Module: Air Quality Warning - Non Wildfire Smoke
 
 library(shiny)
 library(shinydashboard)
