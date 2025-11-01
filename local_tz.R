199214192c43ed54c1abfbfe341d5a5c75ad4f2b
GailR <gail.roth@gov.bc.ca>
2025-11-01 05:16:46 +0100

    create function to identify local time based on params$location

---
+++ local_tz.R
@@ -0,0 +1,9 @@
+local_tz <- function(x) {
+  if (x %in% c("Cranbrook", "Golden", "Invermere")) {
+    "America/Edmonton"  
+  } else if (x %in% c("Fort St John", "Dawson Creek", "Fort Nelson", "Creston")) {
+    "America/Fort_Nelson"
+  } else {
+    "America/Vancouver"
+  }
+}
